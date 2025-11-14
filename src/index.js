22e55f790e782bc053e58d65fe0a118cbab6a096
Ashraf Manna <optimisticashraf@gmail.com>
2025-11-14 11:35:01 +0600

    cors updated

--- src/index.js
+++ src/index.js
@@ -7,10 +7,22 @@ import termsRoutes from "./app/routes/termsRoutes.js";
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: process.env.CLIENT_URL || "http://localhost:5173",
+//   credentials: true,
+// }));
+
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.CLIENT_URL   // Production URL from env
+        : "http://localhost:5173", // Dev URL
+    credentials: true,
+  })
+);
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
